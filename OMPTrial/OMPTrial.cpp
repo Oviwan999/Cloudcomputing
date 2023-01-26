f82c329ad799c49dbe04f865baa33a164f7e3685
Rodrigo R <a01183284@tec.mx>
2023-01-26 17:52:49 -0600

    Subiendo Tarea completa

--- OMPTrial/OMPTrial.cpp
+++ OMPTrial/OMPTrial.cpp
@@ -2,46 +2,61 @@
 //
 
 #include <iostream>
-
+#include <ctime>
 #ifdef _OPENMP
 #include <omp.h>
 #else
 #define omp_get_thread_num() 0
 #endif
+// en este primer paso definimos las librerias a usar al igual que verificamos si necesitaremos llamr OPENMP o no
+//
 
-#define N 24
+#define N 1000
+#define chunk	100
+#define mostrar 10
 
-using namespace std;
-int tid;
-int main()
+// definimos el tamaño de muestra y el tamño de divixion que usaremos
+//
+
+
+void imprimirArreglos(int* a,int* b,int* c)// esta parte  definimos una funcion para mandar imprimier el elemento N de cada arreglo//
 {
-	cout << "Estableciendo la cantidad de Hilos!\n";
-	int nHilos;
-	cout << "Cuantos hilos quieres trabajar: ";
-	cin >> nHilos;
-
-#ifdef _OPENMP
-	omp_set_num_threads(nHilos);
-#endif
-
-#pragma omp parallel private(tid)
+	for (int i = 0; i < mostrar; i++)
 	{
-		tid = omp_get_thread_num();
-		cout << "El thread " << tid << " esta en marcha " << endl;
-		cout << "El thread " << tid << " ha terminado " << endl;
+		std::cout << "A[" << i << "] = " << a[i];
+		std::cout << "\n";
+		std::cout << "B[" << i << "] = " << b[i];
+		std::cout << "\n";
+		std::cout << "La suma Final es C[" << i << "] = " << c[i];
+		std::cout << "\n";
 
 	}
-
-
-
 }
 
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
+void Suma(int* a, int* b, int* c)
+{
+	for (int i = 0; i < N; ++i) 
+	{
+		c[i] = a[i] + b[i];
+	}
+}
 
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
+const int tamano = N+1, MAXIMO = 100, MAXIMO2=100;
+
+int main()
+{
+	int a[tamano] = { 0 };
+	int i;
+	for (i = 0; i < tamano; ++i)
+		a[i] = rand() % MAXIMO;
+	std::cout << "TErmine arreglo A:\n";
+	int b[tamano] = { 0 };
+	for (i = 0; i < tamano; ++i)
+		b[i] = rand() % MAXIMO2;
+	std::cout << "Termine Arreglo B:\n";
+
+	omp_set_num_threads(10);
+	int c[tamano] = { 0 };
+	Suma(a, b, c);
+	imprimirArreglos(a, b, c);
+}
